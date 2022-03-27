5f4ee493c594f53f2482dfe9b52082aa65385e19
amy0706 <1196037680@qq.com>
2022-03-27 12:51:30 +0800

    增加电子书管理页面 新页面开发：页面、路由、菜单

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,7 @@
 import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AdminEbookView from '../views/admin/Admin-EbookView.vue'
+
 
 const routes: Array<RouteRecordRaw> = [
     {
@@ -14,6 +16,11 @@ const routes: Array<RouteRecordRaw> = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    },
+    {
+        path:'/admin/ebook',
+        name:'adminEbook',
+        component:AdminEbookView
     }
 ]
 

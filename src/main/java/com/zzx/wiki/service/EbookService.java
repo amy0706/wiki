591a56867914f44ce07fbafb186e7b93b5a79d10
amy0706 <1196037680@qq.com>
2022-03-27 14:59:04 +0800

    集成PageHelper插件，将mapper日志级别改为trace

--- src/main/java/com/zzx/wiki/service/EbookService.java
+++ src/main/java/com/zzx/wiki/service/EbookService.java
@@ -1,12 +1,15 @@
 package com.zzx.wiki.service;
 
-import com.mysql.cj.util.StringUtils;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zzx.wiki.domain.Ebook;
 import com.zzx.wiki.domain.EbookExample;
 import com.zzx.wiki.mapper.EbookMapper;
 import com.zzx.wiki.req.EbookReq;
 import com.zzx.wiki.resp.EbookResp;
 import com.zzx.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -15,6 +18,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Autowired
     private EbookMapper ebookMapper;
@@ -26,7 +31,13 @@ public class EbookService {
             criteria.andNameLike("%" + req.getName() + "%");
 
         }
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
 
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {

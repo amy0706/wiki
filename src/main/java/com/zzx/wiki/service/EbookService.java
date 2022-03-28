19becf6c6ee2baf71bd69f2f1770b704751f5fa0
amy0706 <1196037680@qq.com>
2022-03-28 17:44:04 +0800

    新增删除电子书功能

--- src/main/java/com/zzx/wiki/service/EbookService.java
+++ src/main/java/com/zzx/wiki/service/EbookService.java
@@ -80,4 +80,11 @@ public class EbookService {
             ebookMapper.updateByPrimaryKey(ebook);
         }
     }
+
+    /**
+     * 删除
+     */
+    public void delete(Long id){
+        ebookMapper.deleteByPrimaryKey(id);
+    }
 }
--- web/src/views/admin/Admin-EbookView.vue
+++ web/src/views/admin/Admin-EbookView.vue
@@ -24,9 +24,16 @@
             <a-button type="primary" @click="edit(record)">
               编辑
             </a-button>
-            <a-button type="danger">
-              删除
-            </a-button>
+            <a-popconfirm
+                title="删除后不可恢复，确认删除?"
+                ok-text="确认"
+                cancel-text="取消"
+                @confirm="handleDelete(record.id)"
+            >
+              <a-button type="danger">
+                删除
+              </a-button>
+            </a-popconfirm>
           </a-space>
         </template>
       </a-table>
@@ -52,7 +59,7 @@
         <a-input v-model:value="ebook.category2Id"/>
       </a-form-item>
       <a-form-item label="描述">
-        <a-input v-model:value="ebook.description" type="text"/>
+        <a-input v-model:value="ebook.description" type="textarea"/>
       </a-form-item>
     </a-form>
   </a-modal>
@@ -161,12 +168,12 @@ export default defineComponent({
         if (data.success) {
           modalVisible.value = false;
           modalLoading.value = false;
+          //重新加载列表
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize
+          });
         }
-        //重新加载列表
-        handleQuery({
-          page: pagination.value.current,
-          size: pagination.value.pageSize
-        });
       });
       message.info('操作成功', 2);
     };
@@ -187,6 +194,22 @@ export default defineComponent({
       ebook.value = {};
     }
 
+    /**
+     * 删除
+     */
+    const handleDelete = (id: number) => {
+      axios.delete("/ebook/delete/" + id).then((response) => {
+        const data = response.data; // data = CommonResp
+        if (data.success) {
+          //重新加载列表
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize
+          });
+        }
+      });
+    };
+
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -203,6 +226,7 @@ export default defineComponent({
 
       edit,
       add,
+      handleDelete,
 
       ebook,
       modalVisible,

93aa2c354d3dfb6b2d747be1000b8186478354a9
amy0706 <1196037680@qq.com>
2022-03-29 12:47:30 +0800

    分类表格按树形结构展示，增加数组转树形数据工具方法

--- web/src/views/admin/Admin-CategoryView.vue
+++ web/src/views/admin/Admin-CategoryView.vue
@@ -20,7 +20,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="categorys"
+          :data-source="level1"
           :loading="loading"
           :pagination="false"
       >
@@ -102,6 +102,19 @@ export default defineComponent({
     ];
 
     /**
+     * 一级分类树，children属性就是二级分类
+     * [{
+     *  id:"",
+     *  name:"",
+     *  children:[{
+     *    id:"",
+     *    name:"",
+     *  }]
+     * }]
+     */
+    const level1 = ref();
+
+    /**
      * 数据查询
      */
     const handleQuery = () => {
@@ -111,6 +124,11 @@ export default defineComponent({
         const data = response.data;
         if (data.success) {
           categorys.value = data.content;
+          console.log("原始数组:", categorys.value);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys.value, 0);
+          console.log("树形结构:", level1);
           //重置分页按钮
         } else {
           message.error(data.message);
@@ -174,7 +192,8 @@ export default defineComponent({
     });
 
     return {
-      categorys,
+      // categorys,
+      level1,
       columns,
       loading,
       handleQuery,
